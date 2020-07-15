d7f33809d2a957158a2d529709933b1bb503a9e5
fanck13 <569441494@qq.com>
2020-07-15 21:07:05 +0800

    add stop watch

---
+++ src/include/stop_watch.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <chrono>
+#include "common.h"
+
+XSTL_NAMESPACE_BEGIN
+
+template <typename T , typename Enable = void>
+class stop_watch;
+
+template<typename T>
+class stop_watch<T, typename std::enable_if<std::is_same<T, std::chrono::microseconds>::value || 
+                                            std::is_same<T, std::chrono::nanoseconds>::value || 
+                                            std::is_same<T, std::chrono::milliseconds>::value>::type>
+{
+    using high_resolution_clock = std::chrono::high_resolution_clock;
+public:
+    stop_watch() {
+        _start = high_resolution_clock::now();
+    }
+
+    auto elapsed() {
+        auto _now = std::chrono::high_resolution_clock::now();
+        return std::chrono::duration_cast<T>(_now - _start).count();
+    }
+
+    void restart(){
+        _start = high_resolution_clock::now();
+    }
+
+    ~stop_watch() { }
+
+private:
+    std::chrono::high_resolution_clock::time_point _start;
+};
+XSTL_NAMESPACE_END
